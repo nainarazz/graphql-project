db57fe9c89587a57cb462ecd578cd78222f2f603
nainarazz <nainarazz@gmail.com>
2018-11-03 23:45:38 +0300

    connected server to mongodb

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
 const schema = require('./schema/schema');
 
 const app = express();
+
+// In production, this should be put in configuration files.
+// For the sake of simplicity, I will hardcode the connection string
+mongoose.connect('mongodb://admin:admin123@ds249873.mlab.com:49873/employee-db-test');
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
 
 app.use('/graphql', graphqlHTTP({
   schema,
