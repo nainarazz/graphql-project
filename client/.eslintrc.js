4c01eec5b5ff7d354c6dbdaa398f70de8b2e72fa
nainarazz <nainarazz@gmail.com>
2018-11-04 11:35:05 +0300

    set up eslint

---
+++ client/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "airbnb",
+    "parser": "babel-eslint",
+    "rules": {
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        "linebreak-style": 0,
+        "react/jsx-one-expression-per-line": 0,
+        "react/destructuring-assignment": 0
+    }
+};
